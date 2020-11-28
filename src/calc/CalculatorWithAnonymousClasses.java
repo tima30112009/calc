bea121dbf6750a47ed9cc4daffb02833801e6d9a
Timur <Timur.zmina@gmail.com>
2020-11-28 16:51:35 +0200

    Add version with anonymous classes

--- src/calc/CalculatorWithAnonymousClasses.java
+++ src/calc/CalculatorWithAnonymousClasses.java
@@ -1,10 +1,41 @@
 package calc;
 
+import static java.lang.String.format;
+
 public class CalculatorWithAnonymousClasses {
     static ArithmeticOperation addition = new ArithmeticOperation() {
         @Override
         public double performOperation(double first, double second) {
-            return 0;
+            double result = first + second;
+            System.out.println(format("%f + %f = %f", first, second, result));
+            return result;
         }
+    };
+    static ArithmeticOperation subtraction = new ArithmeticOperation() {
+        public double performOperation(double first, double second) {
+            double result = first - second;
+            System.out.println(format("%f - %f = %f", first, second, result));
+            return result;
+        }
+    };
+
+    public static double calculate(ArithmeticOperation whatToDo, double first, double second) {
+        return whatToDo.performOperation(first, second);
+    }
+
+    public static void main(String[] args) {
+        calculate(addition, 2.1, 3.4);
+        calculate(addition, 2.1, 3.4);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
